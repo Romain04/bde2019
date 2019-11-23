b603b27783913d6ebc9ad14505812bdc4c26d7b0
Picard Romain <romain.picard@student-cs.fr>
2019-11-23 17:50:56 +0100

    working on responsive

--- js/frontscript.js
+++ js/frontscript.js
@@ -8,7 +8,9 @@
 
 $(function ()
 {
-  var done,
+  var viewportok,
+      done,
+      size = [400,300,200],
       expanded = false,
       uActivated = false,
       ceHeight = $(".CurrentEvent").css('height'),
@@ -17,6 +19,19 @@ $(function ()
       hsec3 = $("#section3").height(),
       hsec4 = $("#section4").height(),
       hsec5 = $("#section5").height();
+
+  if($(window).width() >= 1250)
+  {
+    viewportok = true;
+  }else{
+    viewportok = false;
+    $("aside").fadeOut(200);
+  }
+
+  if($(window).width() <= 1000)
+  {
+    size = ['80%',500,$(window).width()*0.4];
+  }
 
   function getPosition()
   {
@@ -35,26 +50,56 @@ $(function ()
     }
   }
 
+  function hidescreenBegin()
+  {
+    $("#loader").fadeOut(750);
+    $("#hidescreen").off("click", "**", hidescreenBegin);
+  }
+
   function language()
   {
+    var lang = 'fr';
     $("#loader img").fadeOut(200,function(){
       $("#loader").append("<div id='reveal'></div>");
       $("#reveal").fadeOut(0).css({'color':'white'}).html(`
       <h3 id="langTitle">LANGUE / <span class='ang'>LANGUAGE</span></h3>
-      <div class="formcol">
+      <div class="formcol" id="fr">
         <img src="img/frflag.png" alt="Drapeau Fançais / French Flag"/>
         <p>Français</p>
-        <p class="icon-check_box"></p>
+        <p class="lcheckbox icon-check_box"></p>
       </div>
-      <div class="formcol">
+      <div class="formcol" id="en">
         <img src="img/enflag.png" alt="Drapeau Anglais / English Flag"/>
         <p><span class='ang'>English</span></p>
-        <p class="icon-unchecked_box"></p>
+        <p class="lcheckbox icon-unchecked_box"></p>
       </div>
       <button id="langButton" class="button"> OK </button>
       `).delay(200).fadeIn(200,function(){
+        $("#fr").click(function()
+        {
+          if(lang != 'fr')
+          {
+            $("#fr > .lcheckbox").removeClass("icon-unchecked_box").addClass("icon-check_box");
+            $("#en > .lcheckbox").removeClass("icon-check_box").addClass("icon-unchecked_box");
+            lang = 'fr';
+          }
+        });
+        $("#en").click(function()
+        {
+          if(lang != 'en')
+          {
+            $("#en > .lcheckbox").removeClass("icon-unchecked_box").addClass("icon-check_box");
+            $("#fr > .lcheckbox").removeClass("icon-check_box").addClass("icon-unchecked_box");
+            lang = 'en';
+          }
+        });
+        $("#hidescreen").click(hidescreenBegin);
         $("#langButton").click(function()
         {
+          if(lang == 'en')
+          {
+            document.location.href = "/en/home.html";
+          }
           $("#hidescreen").fadeOut(750);
           $("#loader").fadeOut(750);
         });
@@ -110,20 +155,29 @@ $(function ()
     });
   }
 
-  $("#loader").delay(1000).animate({'width': 400,'height': 300,'top':$(window).height()/2 - 150,'left':$(window).width()/2 - 200},700,language);
+  $("#hidescreen").fadeIn(10);
 
-  if($("main").scrollTop() >= 100)
+  $("#loader").delay(1000).animate({'width': size[0],'height': size[1],'top':$(window).height()/2 - size[1]/2,'left':$(window).width()/2 - size[2]},700,language);
+
+  if($("main").scrollTop() >= 100 && viewportok)
   {
     hideAside();
     done = true;
-  }else{
+  }else if(viewportok){
     showAside();
     $("#ArrowUp").fadeOut(750);
     done = false;
   }
 
   $(window).resize(function(){
-    if($("main").scrollTop() >= 100)
+    if($(window).width() >= 1250)
+    {
+      viewportok = true;
+    }else{
+      viewportok = false;
+      $("aside").fadeOut(200);
+    }
+    if($("main").scrollTop() >= 100 && viewportok)
     {
       $(".CurrentEvent").css({'top': $(window).height() - 140});
     }
@@ -131,20 +185,23 @@ $(function ()
 
   $("main").scroll(function()
   {
-    if(!done && $("main").scrollTop() >= 100)
+    if(viewportok)
     {
-      hideAside();
-      $(".CurrentEvent").click(arrow);
-      done = true;
-    }else if(!uActivated && done && $("main").scrollTop() < 100){
-      showAside();
-      $("#ArrowUp").fadeOut(750);
-      done = false;
+      if(!done && $("main").scrollTop() >= 100)
+      {
+        hideAside();
+        $(".CurrentEvent").click(arrow);
+        done = true;
+      }else if(!uActivated && done && $("main").scrollTop() < 100){
+        showAside();
+        $("#ArrowUp").fadeOut(750);
+        done = false;
+      }
     }
-    if(getPosition() === 5)
+    if(getPosition() == 5)
     {
       $("aside").fadeOut(500);
-    }else{
+    }else if(viewportok){
       $("aside").fadeIn(500);
     }
     $("#number").html(getPosition());
